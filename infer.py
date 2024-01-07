1a78c6907d2bb2393ec650031d676eab9b91387b
pawansharmaaaa <18pawansharma72000@gmail.com>
2024-01-08 00:51:47 +0530

    modified:   EDA.ipynb 	modified:   infer.py 	new file:   results/test_prediction.csv

--- infer.py
+++ infer.py
@@ -6,11 +6,11 @@ from keras.models import load_weights
 
 # Model Directory
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_DIR = os.path.join(CURR_DIR, "models")
+MODEL_DIR = os.path.join(CURR_DIR, "model")
 PRO_DIR = os.path.join(CURR_DIR, "processed_data")
 
 # Load model
-model = load_weights(os.path.join(MODEL_DIR, "model0_859.keras"))
+model = load_weights(MODEL_DIR)
 
 # Load test data
 test = pd.read_csv(os.path.join(PRO_DIR, "test.csv"))
@@ -20,21 +20,17 @@ test_data = test.copy()
 # predict
 predictions = model.predict(test_data.values)
 
-prediction_arr=np.array(predictions)
+prediction_arr = np.array(predictions)
 
-target=[]
-for num in test.id:
-    for i in range(1,34):
-        target.append(str(num)+'_y'+str(i))
+target = [str(int(num)) + '_y' + str(int(i)) for num in test.id for i in range(1, 34)]
 
-submission_df=pd.DataFrame(target,columns=['id'])
+submission_df = pd.DataFrame(target, columns=['id'])
 
-prediction_list=[]
-for i in range(len(prediction_arr)):
-    prediction_list.extend(prediction_arr.tolist()[i])
+prediction_list = [pred for sublist in prediction_arr.tolist() for pred in sublist]
 
-pred_df=pd.DataFrame(prediction_list,columns=['pred'])
+pred_df = pd.DataFrame(prediction_list, columns=['pred'])
 
-test_prediction_df=pd.concat([submission_df,pred_df],axis=1)
+test_prediction_df = pd.concat([submission_df, pred_df], axis=1)
 
+os.makedirs(os.path.join(CURR_DIR, "results"), exist_ok=True)
 test_prediction_df.to_csv(os.path.join(CURR_DIR, 'results', 'test_prediction.csv'),index=None)
